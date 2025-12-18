2df3976f0d89579da3c98f6d8f027b3139d55cd2
RPZXD <rpz.tiwa@phichai.ac.th>
2025-12-18 10:45:21 +0700

    Update insert_booking.php

--- teacher/api/insert_booking.php
+++ teacher/api/insert_booking.php
@@ -12,6 +12,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Handle preflight requests
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     http_response_code(200);
